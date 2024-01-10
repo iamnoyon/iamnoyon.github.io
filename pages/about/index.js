a4efce7e35bce204387dd3a4afec133702f7d058
iamnoyon <mdmahfujarrahmannoyon09@gmail.com>
2024-01-11 03:51:16 +0600

    unescaped single quote on texts

--- pages/about/index.js
+++ pages/about/index.js
@@ -137,7 +137,7 @@ const About = () => {
           animate="show"
           exit="hidden"
           className="max-w[500px] mx-auto xl:mx-0 mb-4 xl:mb-10 px-2 xl:px-0">
-          10 years ago, I began freelancing as a developer. Since then, I've done remote work for agencies, counsulted for startups, and collaborated on digital products for business and consumer use.
+          10 years ago, I began freelancing as a developer. Since then, I&apos;ve done remote work for agencies, counsulted for startups, and collaborated on digital products for business and consumer use.
         </motion.p>
         {/* counter */}
         <motion.div
